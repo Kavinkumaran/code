046a0a1e807e088c90689c90bd80e47ae46dab45
Kavinkumaran <51198182+Kavinkumaran@users.noreply.github.com>
2019-08-09 12:03:32 +0500

    Update h3.py

--- h3.py
+++ h3.py
@@ -1,9 +1,14 @@
 n=int(input())
 d=[]
 l=list(map(int,input().split()[:n]))
+flag=-1
 for i in range(n):
     if l[i]==i:
         d.append(i)
+        flag=1
 d.sort()
-for j in d:
-    print(j,end='')
+if flag==1:
+    for j in d:
+        print(j,end='')
+elif flag==-1:
+    print(-1)
